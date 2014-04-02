616688296134b9e5d9cf0bd9f2503c9eb22b9187
Yoway Buorn <yowayb@gmail.com>
2014-04-02 13:50:40 -0700

    script: remove collectors

---
+++ scripts/rm-collectors.py
@@ -0,0 +1,17 @@
+# Remove collectors where field contains some specified string
+#
+# python rm-collectors.py <accessId/email> <accessKey/password> <field> <string>
+
+import sys
+
+from sumologic import SumoLogic
+
+args = sys.argv
+sumo = SumoLogic(args[1], args[2])
+field, string = args[3], args[4]
+cs = sumo.collectors()
+
+for c in cs:
+	if field in c and string in c[field]:
+		cv, _ = sumo.collector(c['id'])
+		print sumo.delete_collector(cv).text
