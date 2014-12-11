42624cd3e28144b7e42ff4a74c728c65a545f0a9
Yoway Buorn <yowayb@gmail.com>
2014-12-11 10:58:09 -0800

    created search method and converted some tabs to spaces per PEP-8

--- scripts/bulk-set.py
+++ scripts/bulk-set.py
@@ -18,24 +18,24 @@ time.sleep(delay)
 f = [{u'regexp': u'\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.(\\d{1,3})', u'mask': u'255', u'filterType': u'Mask', u'name': u'last octet mask'}]
 
 for c in cs:
-	if 'category' not in c or 'bwe' not in c['category'] and 'bwm' not in c['category']:
-		print 'collector: ' + c['name']
-		ss = sumo.sources(c['id'])
-		time.sleep(delay)
-		for s in ss:
-			sv, etag = sumo.source(c['id'], s['id'])
-			time.sleep(delay)
-			svi = sv['source']
-			if 'category' not in svi or 'bwe' not in svi['category'] and 'bwm' not in svi['category']:
-				print 'source: ' + svi['name']
-				svi['filters'] = f
-				r = sumo.update_source(c['id'], sv, etag)
-				print r
-				print r.text
-				time.sleep(delay)
-			#if svi['forceTimeZone'] == False:
-			#	svi['forceTimeZone'] = True
-			#	svi[u'timeZone'] = u'UTC'
-			#	r = sumo.update_source(c['id'], sv, etag)
-			#	print str(r) + ': ' + str(r.text)
-			#	time.sleep(delay)
+    if 'category' not in c or 'bwe' not in c['category'] and 'bwm' not in c['category']:
+        print 'collector: ' + c['name']
+        ss = sumo.sources(c['id'])
+        time.sleep(delay)
+        for s in ss:
+            sv, etag = sumo.source(c['id'], s['id'])
+            time.sleep(delay)
+            svi = sv['source']
+            if 'category' not in svi or 'bwe' not in svi['category'] and 'bwm' not in svi['category']:
+                print 'source: ' + svi['name']
+                svi['filters'] = f
+                r = sumo.update_source(c['id'], sv, etag)
+                print r
+                print r.text
+                time.sleep(delay)
+            #if svi['forceTimeZone'] == False:
+            #    svi['forceTimeZone'] = True
+            #    svi[u'timeZone'] = u'UTC'
+            #    r = sumo.update_source(c['id'], sv, etag)
+            #    print str(r) + ': ' + str(r.text)
+            #    time.sleep(delay)
--- sumologic/sumologic.py
+++ sumologic/sumologic.py
@@ -1,88 +1,91 @@
 from copy import copy
 import json
+
 import requests
 
 class SumoLogic:
+    session = requests.Session()
 
-	endpoint = 'https://api.sumologic.com/api/v1'
-	session = requests.Session()
+    def __init__(self, accessId, accessKey, endpoint='https://api.sumologic.com/api/v1'):
+        self.endpoint = endpoint
+        self.session.auth = (accessId, accessKey)
+        self.session.headers = {'content-type': 'application/json', 'accept': 'application/json'}
 
-	def __init__(self, accessId, accessKey):
-		self.session.auth = (accessId, accessKey)
-		self.session.headers = {'content-type': 'application/json', 'accept': 'application/json'}
+    def delete(self, method, params=None):
+        return self.session.delete(self.endpoint + method, params=params)
 
-	def delete(self, method, params=None):
-		return self.session.delete(self.endpoint + method, params=params)
+    def get(self, method, params=None):
+        return self.session.get(self.endpoint + method, params=params)
 
-	def get(self, method, params=None):
-		return self.session.get(self.endpoint + method, params=params)
+    def post(self, method, params, headers=None):
+        return self.session.post(self.endpoint + method, data=json.dumps(params), headers=headers)
 
-	def post(self, method, params, headers=None):
-		return self.session.post(self.endpoint + method, data=json.dumps(params), headers=headers)
+    def put(self, method, params, headers=None):
+        return self.session.put(self.endpoint + method, data=json.dumps(params), headers=headers)
 
-	def put(self, method, params, headers=None):
-		return self.session.put(self.endpoint + method, data=json.dumps(params), headers=headers)
+    def search(self, query, fromTime=None, toTime=None, timeZone='UTC'):
+        params = {'q': query, 'from': fromTime, 'to': toTime, 'timeZone': timeZone}
+        print params
+        r = self.get('/logs/search', params)
+        return json.loads(r.text)
 
-	def search(self):
-		endpoint = self.endpoint + '/logs/search'
+    def search_job(self, query, fromTime=None, toTime=None, timeZone='UTC'):
+        params = {'query': query, 'from': fromTime, 'to': toTime, 'timeZone': timeZone}
+        print params
+        r = self.post('/search/jobs', params)
+        return json.loads(r.text)
 
-	def search_job(self, query, fromTime, toTime, timeZone=None):
-		params = {'query': query, 'from': fromTime, 'to': toTime, 'timeZone': timeZone}
-		print params
-		r = self.post('/search/jobs', params)
-		return json.loads(r.text)
+    def search_job_status(self, search_job):
+        r = self.get('/search/jobs/' + str(search_job['id']))
+        return json.loads(r.text)
 
-	def search_job_status(self, search_job):
-		r = self.get('/search/jobs/' + str(search_job['id']))
-		return json.loads(r.text)
+    def search_job_messages(self, search_job, limit=None, offset=0):
+        params = {'limit': limit, 'offset': offset}
+        r = self.get('/search/jobs/' + str(search_job['id']) + '/messages', params)
+        return json.loads(r.text)
 
-	def search_job_messages(self, search_job, limit=None, offset=0):
-		params = {'limit': limit, 'offset': offset}
-		r = self.get('/search/jobs/' + str(search_job['id']) + '/messages', params)
-		return json.loads(r.text)
+    def search_job_records(self, search_job, limit=None, offset=0):
+        params = {'limit': limit, 'offset': offset}
+        r = self.get('/search/jobs/' + str(search_job['id']) + '/records', params)
+        return json.loads(r.text)
 
-	def search_job_records(self, search_job, limit=None, offset=0):
-		params = {'limit': limit, 'offset': offset}
-		r = self.get('/search/jobs/' + str(search_job['id']) + '/records', params)
-		return json.loads(r.text)
+    def collectors(self, limit=None, offset=None):
+        params = {'limit': limit, 'offset': offset}
+        r = self.get('/collectors', params)
+        return json.loads(r.text)['collectors']
 
-	def collectors(self, limit=None, offset=None):
-		params = {'limit': limit, 'offset': offset}
-		r = self.get('/collectors', params)
-		return json.loads(r.text)['collectors']
+    def collector(self, collector_id):
+        r = self.get('/collectors/' + str(collector_id))
+        return json.loads(r.text), r.headers['etag']
 
-	def collector(self, collector_id):
-		r = self.get('/collectors/' + str(collector_id))
-		return json.loads(r.text), r.headers['etag']
+    def update_collector(self, collector, etag):
+        headers = {'If-Match': etag}
+        return self.put('/collectors/' + str(collector['collector']['id']), collector, headers)
 
-	def update_collector(self, collector, etag):
-		headers = {'If-Match': etag}
-		return self.put('/collectors/' + str(collector['collector']['id']), collector, headers)
+    def delete_collector(self, collector):
+        return self.delete('/collectors/' + str(collector['collector']['id']))
 
-	def delete_collector(self, collector):
-		return self.delete('/collectors/' + str(collector['collector']['id']))
+    def sources(self, collector_id, limit=None, offset=None):
+        params = {'limit': limit, 'offset': offset}
+        r = self.get('/collectors/' + str(collector_id) + '/sources', params)
+        return json.loads(r.text)['sources']
 
-	def sources(self, collector_id, limit=None, offset=None):
-		params = {'limit': limit, 'offset': offset}
-		r = self.get('/collectors/' + str(collector_id) + '/sources', params)
-		return json.loads(r.text)['sources']
+    def source(self, collector_id, source_id):
+        r = self.get('/collectors/' + str(collector_id) + '/sources/' + str(source_id))
+        return json.loads(r.text), r.headers['etag']
 
-	def source(self, collector_id, source_id):
-		r = self.get('/collectors/' + str(collector_id) + '/sources/' + str(source_id))
-		return json.loads(r.text), r.headers['etag']
+    def update_source(self, collector_id, source, etag):
+        headers = {'If-Match': etag}
+        return self.put('/collectors/' + str(collector_id) + '/sources/' + str(source['source']['id']), source, headers)
 
-	def update_source(self, collector_id, source, etag):
-		headers = {'If-Match': etag}
-		return self.put('/collectors/' + str(collector_id) + '/sources/' + str(source['source']['id']), source, headers)
+    def delete_source(self, collector_id, source):
+        return self.delete('/collectors/' + str(collector_id) + '/sources/' + str(source['source']['id']))
 
-	def delete_source(self, collector_id, source):
-		return self.delete('/collectors/' + str(collector_id) + '/sources/' + str(source['source']['id']))
+    # Danger Zone: this part of REST API likely to change since "dashboard" and "content" overlap
 
-	# Danger Zone: this part of REST API likely to change since "dashboard" and "content" overlap
+    def dashboards(self, include_monitors=False):
+        params = {'monitors': include_monitors}
+        r = self.get('/dashboards', params)
 
-	def dashboards(self, include_monitors=False):
-		params = {'monitors': include_monitors}
-		r = self.get('/dashboards', params)
-
-	def content(self):
-		r = self.get('/content/Private')
+    def content(self):
+        r = self.get('/content/Private')
