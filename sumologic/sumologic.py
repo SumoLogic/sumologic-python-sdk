b68b186c512b6a3002da64493f9699474a2ed992
Melchi Salins <melchi.salins@auspost.com.au>
2016-01-11 02:37:16 +1100

    Adds _get_endpoint method which gets the correct REST endpoint URL when none is supplied.

--- sumologic/sumologic.py
+++ sumologic/sumologic.py
@@ -6,11 +6,32 @@ import requests
 
 class SumoLogic(object):
 
-    def __init__(self, accessId, accessKey, endpoint='https://api.sumologic.com/api/v1'):
-        self.endpoint = endpoint
+    def __init__(self, accessId, accessKey, endpoint=None):
         self.session = requests.Session()
         self.session.auth = (accessId, accessKey)
         self.session.headers = {'content-type': 'application/json', 'accept': 'application/json'}
+        if endpoint is None:
+            self.endpoint = self._get_endpoint()
+        else:
+            self.endpoint = endpoint
+
+    def _get_endpoint(self):
+        """
+        SumoLogic REST API endpoint changes based on the geo location of the client.
+        For example, If the client geolocation is Australia then the REST end point is
+        https://api.au.sumologic.com/api/v1
+
+        When the default REST endpoint (https://api.sumologic.com/api/v1) is used the server
+        responds with a 401 and causes the SumoLogic class instantiation to fail and this very
+        unhelpful message is shown 'Full authentication is required to access this resource'
+
+        This method makes a request to the default REST endpoint and resolves the 401 to learn
+        the right endpoint
+        """
+        self.endpoint = 'https://api.sumologic.com/api/v1'
+        self.response = self.session.get('https://api.sumologic.com/api/v1/collectors')  # Dummy call to get endpoint
+        endpoint = self.response.url.replace('/collectors', '')  # dirty hack to sanitise URI and retain domain
+        return endpoint
 
     def delete(self, method, params=None):
         r = self.session.delete(self.endpoint + method, params=params)
