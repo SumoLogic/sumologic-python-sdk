649d3cb478171a6337fd9528cd3c88417b4dbae6
Yoway Buorn <yowayb@gmail.com>
2016-02-05 12:12:11 -0800

    removed unnecessary email code

--- scripts/search-job-messages.py
+++ scripts/search-job-messages.py
@@ -12,17 +12,13 @@
 # cat query.sumoql | python search-job.py <accessId/email> <accessKey/password> \
 # 1408643380441 1408649380441 PST
 
-from email.mime.text import MIMEText
 import json
-from smtplib import SMTP
 import sys
 import time
 
 from sumologic import SumoLogic
 
 LIMIT = 42
-FROM = 'worker@sumologic.com'
-TO = 'yoway@sumologic.com'
 
 args = sys.argv
 sumo = SumoLogic(args[1], args[2])
@@ -45,13 +41,7 @@ while status['state'] != 'DONE GATHERING RESULTS':
 print status['state']
 
 if status['state'] == 'DONE GATHERING RESULTS':
-	count = status['messageCount']
-	limit = count if count < LIMIT else LIMIT # compensate bad limit check
-	r = sumo.search_job_messages(sj, limit=limit)
-	msg = MIMEText(json.dumps(r))
-	msg['From'] = FROM
-	msg['To'] = TO
-	msg['Subject'] = 'Search Jobs Results'
-	smtp = SMTP('localhost')
-	smtp.sendmail(FROM, [TO], msg.as_string())
-	smtp.quit()
+    count = status['messageCount']
+    limit = count if count < LIMIT else LIMIT # compensate bad limit check
+    r = sumo.search_job_messages(sj, limit=limit)
+    print r
--- scripts/search-job.py
+++ scripts/search-job.py
@@ -12,17 +12,16 @@
 # cat query.sumoql | python search-job.py <accessId/email> <accessKey/password> \
 # 1408643380441 1408649380441 PST
 
-from email.mime.text import MIMEText
 import json
-from smtplib import SMTP
 import sys
 import time
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 from sumologic import SumoLogic
 
 LIMIT = 42
-FROM = 'worker@sumologic.com'
-TO = 'yoway@sumologic.com'
 
 args = sys.argv
 sumo = SumoLogic(args[1], args[2])
@@ -36,23 +35,16 @@ sj = sumo.search_job(q, fromTime, toTime, timeZone)
 
 status = sumo.search_job_status(sj)
 while status['state'] != 'DONE GATHERING RESULTS':
-	if status['state'] == 'CANCELLED':
-		break
-	time.sleep(delay)
-	delay *= 2
-	status = sumo.search_job_status(sj)
+    if status['state'] == 'CANCELLED':
+        break
+    time.sleep(delay)
+    delay *= 2
+    status = sumo.search_job_status(sj)
 
 print status['state']
 
 if status['state'] == 'DONE GATHERING RESULTS':
-	count = status['recordCount']
-	limit = count if count < LIMIT else LIMIT # compensate bad limit check
-	r = sumo.search_job_records(sj, limit=limit)
-	print r
-	msg = MIMEText(json.dumps(r))
-	msg['From'] = FROM
-	msg['To'] = TO
-	msg['Subject'] = 'Search Jobs Results'
-	smtp = SMTP('localhost')
-	smtp.sendmail(FROM, [TO], msg.as_string())
-	smtp.quit()
+    count = status['recordCount']
+    limit = count if count < LIMIT else LIMIT # compensate bad limit check
+    r = sumo.search_job_records(sj, limit=limit)
+    print r
