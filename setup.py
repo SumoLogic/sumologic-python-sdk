74e04d3304126fa58e74e6f386907e5b6983f0e5
Melchi Salins <salinsm@mpdmmac-19.local>
2016-02-14 17:18:51 +1100

    Fixed version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sumologic-sdk",
-    version="0.1.5",
+    version="0.1.4",
     packages=find_packages(),
     install_requires=['requests>=2.2.1'],
     # PyPI metadata
