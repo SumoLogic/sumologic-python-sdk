2ca5e97e4f7514a98cdf8ddd372a9ff00ea8b8a1
Duc Ha <duchatran@gmail.com>
2017-01-31 14:38:48 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sumologic-sdk",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
     install_requires=['requests>=2.2.1'],
     # PyPI metadata
