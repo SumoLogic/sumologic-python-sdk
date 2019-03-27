f6054bad09d694337b4fe6c14f707b466ec33bdf
Duc Ha <duchatran@gmail.com>
2019-03-27 12:05:25 -0700

    Update version to 0.1.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sumologic-sdk",
-    version="0.1.8",
+    version="0.1.9",
     packages=find_packages(),
     install_requires=['requests>=2.2.1'],
     # PyPI metadata
