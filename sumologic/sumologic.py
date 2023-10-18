d748fb5c387a2676b0461151e045813aa308a294
pmorgan <peterjmorgan@gmail.com>
2023-10-18 14:59:20 -0700

    remove unnecessary comment

--- sumologic/sumologic.py
+++ sumologic/sumologic.py
@@ -291,8 +291,6 @@ class SumoLogic(object):
         return self.post('/content/%s/move?destinationFolderId=%s' % (content_id, destination_folder), params=None,
                          version='v2')
 
-    # Additions
-
     def get_content_item_by_path(self, path):
         return self.get('/content/path?path=%s' % (path), params=None, version='v2')
 
