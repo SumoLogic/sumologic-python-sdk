8c687af3695d6c5f5e06fa3e8ca5ab6561b7919b
Chris Schafer <51336089+upstart-swiss@users.noreply.github.com>
2023-12-01 16:21:31 -0700

    Set autoParsingMode to its default vs a broken value

--- sumologic/sumologic.py
+++ sumologic/sumologic.py
@@ -148,7 +148,7 @@ class SumoLogic(object):
         return r
 
     # Logs Search
-    def search_job(self, query, fromTime=None, toTime=None, timeZone='UTC', byReceiptTime=None, autoParsingMode=None):
+    def search_job(self, query, fromTime=None, toTime=None, timeZone='UTC', byReceiptTime=None, autoParsingMode='Manual'):
         params = {'query': query, 'from': fromTime, 'to': toTime, 'timeZone': timeZone, 'byReceiptTime': byReceiptTime, 'autoParsingMode': autoParsingMode}
         r = self.post('/search/jobs', params)
         return json.loads(r.text)
